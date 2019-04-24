0a19673c0ba33cd5540dc5a8362aca2549461c70
VinhLoiIT <vinhloiit1327@gmail.com>
2019-04-24 20:30:07 +0700

    fix code to draw prettier

--- seminar/r_score_normal.py
+++ seminar/r_score_normal.py
@@ -1,49 +1,48 @@
 import numpy as np
+import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def least_square_estimation(x:np.ndarray,y:np.ndarray):
-    inv = np.linalg.inv(x.transpose().dot(x))
-    return inv.dot(x.transpose()).dot(y)
-    # return x.transpose().dot(x).inv().dot(x.transpose).dot(y)
+def least_square_estimation(z:np.ndarray,y:np.ndarray):
+    inv = np.linalg.inv(z.transpose().dot(z))
+    return inv.dot(z.transpose()).dot(y)
 
 seed = 42
 np.random.seed(seed) # make repeate random values
 
 n = 20
-z = np.linspace(-10,10,n)
-epsilon = np.random.randn(n)
+z = np.linspace(-10,10,n).transpose().reshape(-1,1)
+epsilon = np.random.randn(n,1) # n rows, 1 column
 y = 2*z + epsilon
+
+# plot data
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(z,y, label="real data")
+ax.set_title("Linear Regression")
 ax.set_xlabel("z")
 ax.set_ylabel("y")
+ax.scatter(z,y, label="real data")
 ax.legend()
-plt.show()
+fig.savefig("real_data.png")
 
-# df = pd.DataFrame({"z":z, "y": y})
-# print(df)
-# df.to_csv("data_normal.csv")
 
-z_0 = np.ones((n))
-
-Z = np.vstack((z_0,z)).transpose()
-Y = y.transpose()
+z_0 = np.ones((n,1))
+Z = np.hstack((z_0,z))
+Y = y
 b_hat = least_square_estimation(Z,Y)
 print("Beta =", b_hat)
 
-y_plot = np.vstack((z_0, z)).transpose().dot(b_hat)
-plt.plot(z,y_plot, color='r', label="y={:.02f}+{:.02f}z".format(b_hat[0], b_hat[1]))
-# plt.scatter(z,y_plot, color='g')
+# plot line
+y_plot = Z.dot(b_hat)
+b_hat_ravel = b_hat.ravel()
+ax.plot(z,y_plot, color='r', label="y = {:.02f} + {:.02f}z".format(b_hat_ravel[0], b_hat_ravel[1]))
+ax.legend()
+fig.savefig("least_square_estimated.png")
 
+# show r_square score
 r_squares = np.var(y_plot)/np.var(y)
 props = dict(boxstyle="round", facecolor="wheat", alpha=0.5)
-plt.text(-10,-2,"r-squares={:.02f}".format(r_squares), bbox=props)
-
-plt.legend()
-plt.xlabel("z")
-plt.ylabel("y")
-plt.title("Hồi quy tuyến tính 1 biến")
+ax.text(-10,-2,"r-squares={:.02f}".format(r_squares), bbox=props)
+fig.savefig("r_square_1.png")
 
 plt.show()
--- seminar/r_score_parabol.py
+++ seminar/r_score_parabol.py
@@ -1,11 +1,11 @@
 import numpy as np
+import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 
 def f(x):
     return -3*x**2+4*x-1
-    # return 2*x
 
 def least_square_estimation(x:np.ndarray,y:np.ndarray):
     inv = np.linalg.inv(x.transpose().dot(x))
@@ -17,59 +17,69 @@ np.random.seed(seed) # make repeate random values
 # rand_error = np.random.randn(10)
 # print(rand_error)
 
-x = np.linspace(-10,10,50)
-y = f(x)
-z_0 = np.ones((50))
+n = 20
+z_1 = np.linspace(-10,10,n).transpose().reshape(-1,1)
+epsilon = np.random.randn(n,1) # n rows, 1 column
+y = -3*z_1**2+4*z_1-1 #+ epsilon
 
-Z = np.vstack((z_0,x)).transpose()
-Y = y.transpose()
+# plot data
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_title("Linear Regression")
+ax.set_xlabel("z")
+ax.set_ylabel("y")
+ax.scatter(z_1,y, label="real data")
+ax.legend()
+fig.savefig("real_data_2.png")
+
+z_0 = np.ones((n,1))
+Z = np.hstack((z_0,z_1))
+Y = y
 b_hat = least_square_estimation(Z,Y)
-print(b_hat.shape)
-print(b_hat)
+print("Beta =", b_hat)
 
-plt.scatter(x,y)
-y_plot = np.vstack((np.ones((50)), x)).transpose().dot(b_hat)
-print(y_plot)
-plt.plot(x,y_plot, color='r')
-# plt.scatter(x,y_plot, color='g')
-
+y_plot = Z.dot(b_hat)
+b_hat_ravel = b_hat.ravel()
+ax.plot(z_1,y_plot, color='r', label="y = {:.02f} + {:.02f}z".format(b_hat_ravel[0], b_hat_ravel[1]))
+ax.legend()
+fig.savefig("least_square_estimated_2.png")
 
 r_squares = np.var(y_plot)/np.var(y)
 props = dict(boxstyle="round", facecolor="wheat", alpha=0.5)
-plt.text(-10,-2,"r-squares = {:.02f}".format(r_squares), bbox=props)
-print(r_squares)
-# plt.legend()
+ax.text(-10,-2,"r-squares={:.02f}".format(r_squares), bbox=props)
+fig.savefig("r_square_2.png")
 
-plt.show()
-
-Z_2 = np.vstack((z_0,x,x**2)).transpose()
-Y = y.transpose()
-b_hat = least_square_estimation(Z_2, Y)
+z_2 = z_1**2
+Z = np.hstack((Z, z_2))
+b_hat = least_square_estimation(Z, Y)
 # print(b_hat.reshape(1,-1))
-print(b_hat)
+print("Beta_2 =",b_hat)
+
+plt.close()
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-ax.scatter(xs=x, ys=x**2, zs= f(x), s=40, alpha=1, edgecolors='w', color='r')
+ax.scatter(xs=z_1, ys=z_2, zs=y, s=40, alpha=1, edgecolors='w', color='r')
+fig.savefig("real_data_2_after.png")
 
 xx1, xx2 = np.meshgrid(
-    np.linspace(x.min(), x.max(), num=50),
-    np.linspace((x**2).min(), (x**2).max(), num=50)
+    np.linspace(z_1.min(), z_1.max(), num=n),
+    np.linspace(z_2.min(), z_2.max(), num=n)
 )
-XX = np.column_stack([np.ones(50**2), xx1.ravel(), xx2.ravel()])
+XX = np.column_stack([np.ones((n,n)).ravel(), xx1.ravel(), xx2.ravel()])
 
-print("xx", XX.shape)
-Z = XX.transpose()
-print(Z.shape)
-yy = b_hat.dot(Z)
-ax.plot_surface(xx1, xx2, yy.reshape(xx1.shape), color='b')
+Z = XX
+yy = Z.dot(b_hat)
+ax.plot_surface(xx1, xx2, yy.reshape(xx1.shape))
+fig.savefig("least_square_estimated_2_after.png")
 
 r_squares = np.var(yy)/np.var(Y)
 props = dict(boxstyle="round", facecolor="wheat", alpha=0.5)
 ax.text(-10,-2,5,s="r-squares = {:.02f}".format(r_squares), bbox=props)
-# ax.set_xlabel(x.columns[0])
-# ax.set_ylabel(x.columns[1])
-# ax.set_zlabel(y.name)
+ax.set_xlabel("z")
+ax.set_ylabel("z^2")
+ax.set_zlabel("y")
+fig.savefig("r_square_2_after.png")
 
 plt.show()
